f04524e4171bb3001d10d91ff8bf2f0cbdd55f99
Om Naidu <79000269+cloudcreatr@users.noreply.github.com>
2025-03-03 11:43:41 +0000

    yeah

--- p-m/src/index.ts
+++ p-m/src/index.ts
@@ -1,41 +1,17 @@
+import { Hono } from 'hono'
+
+
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/bun-sqlite";
-import { eq } from "drizzle-orm";
-import { usersTable } from "./db/schema";
 
 const db = drizzle(process.env.DB_FILE_NAME!);
 
-async function main() {
-  const user: typeof usersTable.$inferInsert = {
-    name: "John",
-    age: 30,
-    email: "john@example.com",
-  };
+//omm
 
-  await db.insert(usersTable).values(user);
-  console.log("New user created!");
+const app = new Hono()
 
-  const users = await db.select().from(usersTable);
-  console.log("Getting all users from the database: ", users);
-  /*
-  const users: {
-    id: number;
-    name: string;
-    age: number;
-    email: string;
-  }[]
-  */
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
 
-  await db
-    .update(usersTable)
-    .set({
-      age: 31,
-    })
-    .where(eq(usersTable.email, user.email));
-  console.log("User info updated!");
-
-  await db.delete(usersTable).where(eq(usersTable.email, user.email));
-  console.log("User deleted!");
-}
-
-main();
+export default app
